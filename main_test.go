b69b967b057346499d5562ae76775542bf4dd226
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 14:42:51 -0500

    Tiny steps

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import . "gopkg.in/check.v1"
+
+type MainSuite struct {
+	BaseSuite
+}
+
+var _ = Suite(&MainSuite{})
+
+func (s *MainSuite) TestMain(c *C) {
+	c.Assert(
+		func() {
+			main()
+		},
+		Not(PanicMatches),
+		"*",
+	)
+}
