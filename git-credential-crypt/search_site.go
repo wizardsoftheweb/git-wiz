b0b68538e1a95aa77b6b78e7af092f62cbcf621e
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 08:18:03 -0500

    Built and tested an input parser method with solid regex

---
+++ git-credential-crypt/search_site.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"regexp"
+)
+
+const testContent = `\
+protocol=https
+password=qqq
+path=seven
+host=rick.james
+username=bobby
+golang=something
+`
+
+var PatternSingleInputLine = regexp.MustCompile(`(?m)^\s*(protocol|username|host|path)\s*=\s*(.*)\s*$`)
+
+func ParseSearchInput(input string) map[string]string {
+	matches := PatternSingleInputLine.FindAllStringSubmatch(input, -1)
+	incoming := make(map[string]string)
+	for _, value := range matches {
+		incoming[value[1]] = value[2]
+	}
+	return incoming
+}
---
+++ git-credential-crypt/search_site_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"strings"
+
+	. "gopkg.in/check.v1"
+)
+
+type SearchSiteSuite struct {
+	BaseSuite
+}
+
+var _ = Suite(&SearchSiteSuite{})
+
+func (s *SearchSiteSuite) SetUpTest(c *C) {
+}
+
+func (s *SearchSiteSuite) TearDownTest(c *C) {
+}
+
+func (s *SearchSiteSuite) TestSearchInput(c *C) {
+	matrix := []struct {
+		input      []string
+		finalValue map[string]string
+	}{
+		{
+			[]string{
+				"protocol = https",
+				"username = rick",
+				"password = waste",
+				"host = james",
+				"path = couch",
+			},
+			map[string]string{
+				"protocol": "https",
+				"username": "rick",
+				"host":     "james",
+				"path":     "couch",
+			},
+		},
+	}
+	for _, entry := range matrix {
+		input := strings.Join(entry.input, "\n")
+		results := ParseSearchInput(input)
+		c.Assert(results, DeepEquals, entry.finalValue)
+	}
+}
