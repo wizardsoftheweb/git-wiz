263e0c13909c12d1633504b4312a0d9b46f4674a
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 03:37:40 -0500

    Add more demoing to main

--- git-credential-crypt/main.go
+++ git-credential-crypt/main.go
@@ -3,5 +3,9 @@ package main
 func main() {
 	panic(Execute())
 
-	// store := NewStoreFromDisk()
+	// store := NewStoreFromDisk("~/.git-credentials")
+	// fmt.Println(store)
+	// for _, site := range store.Sites {
+	// 	fmt.Println(site)
+	// }
 }
