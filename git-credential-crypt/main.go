458a7c991b29360ce7486da90911aa306307fcf2
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 05:37:14 -0500

    Tidy up to move on

--- git-credential-crypt/main.go
+++ git-credential-crypt/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
-	// panic(Execute())
+	panic(Execute())
 
-	store := NewStoreFromDisk("~/.git-credentials")
-	fmt.Println(store)
-	for _, site := range store.Sites {
-		fmt.Println(site)
-	}
-	site := store.Get(map[string]string{"host": "api.*.com"})
-	fmt.Println(site)
+	// store := NewStoreFromDisk("~/.git-credentials")
+	// fmt.Println(store)
+	// for _, site := range store.Sites {
+	// 	fmt.Println(site)
+	// }
+	// site := store.Get(map[string]string{"host": "api.*.com"})
+	// fmt.Println(site)
 }
