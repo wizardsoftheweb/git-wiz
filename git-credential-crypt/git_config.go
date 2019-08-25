8fe2f0da6848f4f765a829cdea7c6796f39f716e
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 12:17:46 -0500

    Scaffold the environment config collection

--- git-credential-crypt/git_config.go
+++ git-credential-crypt/git_config.go
@@ -1,5 +1,35 @@
 package main
 
-func DoPathsMatter() bool {
-	return true
+import (
+	"os/exec"
+	"regexp"
+	"strconv"
+)
+
+var DoPathsMatter = PathConfigMonitor
+
+func PathConfigMonitor() bool {
+	blob := CollectGitConfig()
+	return CheckPathValue(blob)
+}
+
+// var PatternGitConfigLine = regexp.MustCompile(`(?mi)^credential\.(.*?)\.*(helper|usehttppath|username)\s*(.*)\s*$`)
+var PatternConfigUseHttpPath = regexp.MustCompile(`(?mi)^credential\.(.*?)\.*(usehttppath)\s*(.*)\s*$`)
+
+func CollectGitConfig() string {
+	commandArgs := []string{"config", "--get-regexp", "credential*"}
+	result, err := exec.Command("git", commandArgs...).CombinedOutput()
+	whereErrorsGoToDie(err)
+	return string(result)
+}
+
+func CheckPathValue(blob string) bool {
+	pathsMatter := false
+	matches := PatternConfigUseHttpPath.FindAllStringSubmatch(blob, -1)
+	for _, match := range matches {
+		if "" != match[3] {
+			pathsMatter, _ = strconv.ParseBool(match[3])
+		}
+	}
+	return pathsMatter
 }
