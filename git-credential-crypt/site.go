fafa4d23ab6434f6aa9c76b5b5f484d7743f6210
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 00:24:36 -0500

    Define search function

--- git-credential-crypt/site.go
+++ git-credential-crypt/site.go
@@ -17,11 +17,12 @@ const (
 )
 
 type Site struct {
-	Protocol string
-	Host     string
-	Username string
-	Password string
-	Url      string
+	Protocol       string
+	Host           string
+	Username       string
+	Password       string
+	Url            string
+	sliceForSearch [4]string
 }
 
 func NewSite(url string) *Site {
@@ -41,4 +42,24 @@ func (s *Site) parseUrl() {
 	s.Username = s.decodeComponent(matches[PositionSiteUsername+1])
 	s.Password = s.decodeComponent(matches[PositionSitePassword+1])
 	s.Host = s.decodeComponent(matches[PositionSiteHost+1])
+	s.sliceForSearch = [4]string{
+		matches[PositionSiteProtocol+1],
+		matches[PositionSiteUsername+1],
+		matches[PositionSitePassword+1],
+		matches[PositionSiteHost+1],
+	}
+}
+
+func (s *Site) IsAMatch(activated [4]bool, query [4]string) bool {
+	for index, active := range activated {
+		if int(PositionSitePassword) == index {
+			continue
+		}
+		if active {
+			if query[index] != s.sliceForSearch[index] {
+				return false
+			}
+		}
+	}
+	return true
 }
