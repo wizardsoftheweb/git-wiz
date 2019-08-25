e95661c3f260118055d192e390a7473ff5873a45
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 01:13:08 -0500

    Test the search permutations

--- git-credential-crypt/site_test.go
+++ git-credential-crypt/site_test.go
@@ -8,11 +8,23 @@ import (
 
 type SiteSuite struct {
 	BaseSuite
+	defaultValue  string
+	nothingActive [4]bool
+	allActive     [4]bool
+	emptyQuery    [4]string
+	fullQuery     [4]string
+	site          *Site
 }
 
 var _ = Suite(&SiteSuite{})
 
 func (s *SiteSuite) SetUpTest(c *C) {
+	s.defaultValue = "qqq"
+	s.nothingActive = [4]bool{false, false, false, false}
+	s.allActive = [4]bool{true, true, true, true}
+	s.emptyQuery = [4]string{"", "", "", ""}
+	s.fullQuery = [4]string{s.defaultValue, s.defaultValue, s.defaultValue, s.defaultValue}
+	s.site = &Site{}
 }
 
 func (s *SiteSuite) TearDownTest(c *C) {
@@ -32,4 +44,15 @@ func (s *SiteSuite) TestNewSite(c *C) {
 	c.Assert(site.Username, Equals, input[PositionSiteUsername])
 	c.Assert(site.Password, Equals, input[PositionSitePassword])
 	c.Assert(site.Host, Equals, input[PositionSiteHost])
+}
+
+func (s *SiteSuite) TestIsAMatchAllPermutations(c *C) {
+	for _, entry := range siteSearchTestMatrix {
+		s.site.sliceForSearch = entry.siteValues
+		c.Assert(
+			s.site.IsAMatch(entry.activated, entry.query),
+			Equals,
+			entry.result,
+		)
+	}
 }
