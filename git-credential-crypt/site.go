f6fb5afb584f6656a5155c2beb0a750e432a3469
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-24 21:03:09 -0500

    Flesh out individual file lines

--- git-credential-crypt/site.go
+++ git-credential-crypt/site.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"net/url"
 	"regexp"
 )
 
-var PatternUrl = regexp.MustCompile(`^\s*(https?)(://)?([^/]+)/?(.+)?\s*$`)
+var PatternSiteEntry = regexp.MustCompile(`^\s*(.*?)(://)(.*):(.*)@(.*)\s*$`)
+
+type MatchPositionSite int
+
+const (
+	PositionSiteFull MatchPositionSite = iota
+	PositionSiteProtocol
+	PositionSiteUsername
+	PositionSitePassword
+	PositionSiteHost
+)
 
 type Site struct {
 	Protocol string
 	Host     string
-	Path     string
 	Username string
 	Password string
 	Url      string
+}
+
+func NewSite(url string) *Site {
+	site := Site{Url: url}
+	site.parseUrl()
+	return &site
+}
+
+func (s *Site) decodeComponent(value string) string {
+	decoded, _ := url.QueryUnescape(value)
+	return decoded
+}
+
+func (s *Site) parseUrl() {
+	matches := PatternSiteEntry.FindAllStringSubmatch(s.Url, -1)[0]
+	s.Protocol = s.decodeComponent(matches[PositionSiteProtocol])
+	s.Username = s.decodeComponent(matches[PositionSiteUsername])
+	s.Password = s.decodeComponent(matches[PositionSitePassword])
+	s.Host = s.decodeComponent(matches[PositionSiteHost])
 }
