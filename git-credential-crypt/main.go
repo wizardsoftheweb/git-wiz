43013d6b0a98c0acd5c74e6ed758a459a7c12908
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 05:31:33 -0500

    Learn more about the back and forth

--- git-credential-crypt/main.go
+++ git-credential-crypt/main.go
@@ -1,11 +1,15 @@
 package main
 
-func main() {
-	panic(Execute())
+import "fmt"
 
-	// store := NewStoreFromDisk("~/.git-credentials")
-	// fmt.Println(store)
-	// for _, site := range store.Sites {
-	// 	fmt.Println(site)
-	// }
+func main() {
+	// panic(Execute())
+
+	store := NewStoreFromDisk("~/.git-credentials")
+	fmt.Println(store)
+	for _, site := range store.Sites {
+		fmt.Println(site)
+	}
+	site := store.Get(map[string]string{"host": "api.*.com"})
+	fmt.Println(site)
 }
