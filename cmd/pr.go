25ddcb29c03d55b3859d8f0f23499e06429dc96c
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 21:53:21 -0500

    Test by changing the prCmd run block

--- cmd/pr.go
+++ cmd/pr.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/spf13/cobra"
 )
 
@@ -12,10 +9,8 @@ var prCmd = &cobra.Command{
 	Short: "PRs through the CLI where your git flow already is",
 	Long:  "Just GH for now. This may or may not ever be finished.",
 	Run: func(cmd *cobra.Command, args []string) {
-		Demo = WotwPrRequest{}
-		client := &http.Client{}
-		body := getResource(client, "")
-		fmt.Println(string(body))
+		rawPr := compileSuggestedPrBody()
+		sharePr(rawPr)
 	},
 }
 
