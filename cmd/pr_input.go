6499d64adcbaaa046fcc56de53d986c00bc2c314
CJ Harries <cj@wizardsoftheweb.pro>
2019-08-25 21:52:55 -0500

    Compile discovery and prep to share with consumer

---
+++ cmd/pr_input.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+)
+
+func compileSuggestedPrBody() *GitHubPrRequest {
+	discovery := CompletePrDiscovery()
+	return &GitHubPrRequest{
+		Title:               discovery.assumedCurrentBranch,
+		Base:                discovery.assumedCurrentBranch,
+		Head:                discovery.suggestedHead,
+		Body:                "",
+		MaintainerCanModify: true,
+	}
+}
+
+func sharePr(pr *GitHubPrRequest) {
+	fmt.Println(pr)
+}
+
+func compileSuggestedReviewBody(owner, repo string) *GithubReviewRequest {
+	reviewers := CompleteReviewDiscovery(owner, repo)
+	return &GithubReviewRequest{
+		Reviewers: reviewers,
+	}
+}
